d1ee51fccabf12827785dd4a1d76adb4e5bf6a47
valor4valkyrie <c.george.pemberton@gmail.com>
2019-10-09 21:56:30 -0500

    first commit

---
+++ UpdateReboot.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt-get update && sudo apt-get dist-upgrade -y && sudo apt install --install-recommends linux-generic-hwe-18.04 xserver-xorg-hwe-18.04 -y && sudo apt-get autoremove -y && sudo apt-get autoclean -y && sudo apt-get clean && reboot
