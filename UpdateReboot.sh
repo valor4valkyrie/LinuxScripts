72feaef2b4229501ee3977c9acbfeb98e3a00b79
valor4valkyrie <c.george.pemberton@gmail.com>
2021-01-01 21:06:38 -0600

    Removed 'get' from some commands and removed 'clean'

--- UpdateReboot.sh
+++ UpdateReboot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y && sudo apt-get autoclean -y && sudo apt-get clean && reboot
+sudo apt-get update && sudo apt-get dist-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y && reboot
