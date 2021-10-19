d6424df50533f74f54380cb04add4d77fa983e5c
Chris Pemberton <c.george.pemberton@gmail.com>
2021-10-18 19:04:42 -0500

    Added logging to the scripts

--- UpdateReboot.sh
+++ UpdateReboot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt-get update && apt-get dist-upgrade -y && apt autoremove -y && apt autoclean -y && reboot
+date > /var/log/UpdateLog.txt && apt-get update >> /var/log/UpdateLog.txt && apt-get dist-upgrade -y >> /var/log/UpdateLog.txt && apt autoremove -y >> /var/log/UpdateLog.txt && apt autoclean -y >> /var/log/UpdateLog.txt && reboot
--- UpdateShutdown.sh
+++ UpdateShutdown.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt-get update && apt-get dist-upgrade -y && apt autoremove -y && apt autoclean -y && shutdown now
+date > /var/log/UpdateLog.txt && apt-get update >> /var/log/UpdateLog.txt && apt-get dist-upgrade -y >> /var/log/UpdateLog.txt && apt autoremove -y >> /var/log/UpdateLog.txt && apt autoclean -y >> /var/log/UpdateLog.txt && shutdown now
