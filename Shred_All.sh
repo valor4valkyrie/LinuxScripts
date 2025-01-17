16d64a1865c1fcdce19187943b1a00a88e18e5ff
valor4valkyrie <c.george.pemberton@gmail.com>
2025-01-16 19:07:46 -0600

    Updated shred logic

--- Shred_All.sh
+++ Shred_All.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 FILES="${1}"
 
-for f in $(find "$FILES" -type f); do shred -vzu -n10 "$f"; done
+for f in $(find "$FILES" -type f);
+do
+  shred -vzu -n10 "$f";
+done
 
-find "${FILES}" -type d | while read dir; do rm -r "$dir"; done
+find "${FILES}" -type d | while read dir;
+do
+  rm -r "$dir";
+done
