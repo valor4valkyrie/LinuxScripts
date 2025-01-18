cb4aeb7f7db11321fb155b787e1f0ce6ea41c307
Christopher Pemberton <c.george.pemberton@gmail.com>
2025-01-17 19:54:35 -0600

    Accounting for spaces in file and directory names.

--- Shred_All.sh
+++ Shred_All.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 FILES="${1}"
-
+IFS=$'\n'
 for f in $(find "$FILES" -type f);
 do
+  SPACELESS_FILES=$(printf %q "$f");
   shred -vzu -n10 "$f";
 done
 
 find "${FILES}" -type d | while read dir;
 do
-  rm -r "$dir";
+  SPACELESS_DIR=$(printf %q "$dir");
+  rm -r "$SPACELESS_DIR";
 done
